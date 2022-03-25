ad2581bfffd176af9e83e689670c6a224928d820
Nikhil A <97142198+NIKHILAV-123@users.noreply.github.com>
2022-03-25 14:16:59 +0000

    p2final.c

--- p2final.c
+++ p2final.c
@@ -1,44 +1,40 @@
 #include<stdio.h>
-
 int input_side()
 {
   int side;
-  printf("enter a side of the triangle:\n");
-  scanf("%d", &side);
+  printf("Enter a side of triangle:\n");
+  scanf("%d\n",&side);
   return side;
-  
 }
-int check_scalene(int a, int b, int c)
+int check_scalene(int a,int b,int c)
 {
-  if (a != b && a != c && b !=c)
+  if(a != b && a != c && b != c)
   {
     return 1;
   }
   return 0;
 }
-void output(int a, int b, int c, int isScalene)
+void output(int a,int b,int c,int isScalene)
 {
   if(isScalene)
-    
   {
-    printf("the triangle with sides %d, %d and %d 
+    printf("the triangle with  sides %d,%d and %d 
     isscalene\n",a,b,c);
   }
   else
   {
-    printf("the triangle with sides %d, %d and %d 
-    is not scalene\n",a,b,c);
+    printf("the triangle with sides %d,%d and 
+    %d isnot scalene\n",a,b,c);
   }
 }
-
 int main()
 {
-  int AB = input_side();
-  int BC = input_side();
-  int CA = input_side();
-
-  int res = check_scalene(AB,BC,CA);
+  
+  int AB=input_side();
+  int BC=input_side();
+  int CA=input_side();
+  
+  int res=check_scalene(AB,BC,CA);
   output(AB,BC,CA,res);
   return 0;
-  
 }
